1ffc1c5ca111a35d443d760ba06c4263d69a58ee
Akshay Varma <varmaakshay2020@gmail.com>
2025-07-09 02:53:14 +0530

    Initial commit of Preet Designer Boutique website

---
+++ index.js
@@ -0,0 +1,23 @@
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+
+// Swiper carousel setup
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
